787a68f1f18979eeb234d00666880dbcd449c7e6
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2025-08-01 16:23:21 -0400

    binki-sharefile-autologin-1.0.0

---
+++ README.md
@@ -0,0 +1,5 @@
+Automatically click through the Sharefile login screens, optimized for password managers or autocomplete or pasting usernames and passwords and MFA codes.
+
+This prefers to use Authenticator if it is shown as an option when signing in. If you need to use another MFA service such as voice or text, you must temporarily disable this script.
+
+[Install](binki-sharefile-autologin.user.js?raw=1)
---
+++ binki-sharefile-autologin.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name binki-sharefile-autologin
+// @version 1.0.0
+// @match https://auth.sharefile.io/*
+// @require https://github.com/binki/binki-userscript-when-element-query-selector-async/raw/0a9c204bdc304a9e82f1c31d090fdfdf7b554930/binki-userscript-when-element-query-selector-async.js
+// @require https://github.com/binki/binki-userscript-when-input-completed/raw/19b92186c42958221c62d0cf1171e2655e7a7514/binki-userscript-when-input-completed.js
+// ==/UserScript==
+
+// Try to avoid touching pages which are irrelevant.
+if (/^[^/]+\/\/[^/]+\/[^/]+\/login(?:$|\?)/.test(document.URL)) {
+  // Username. This is skipped if the user chose to have the email remembered, so run this as a fire-and-forget.
+  (async () => {
+    const rememberMeInput = await whenElementQuerySelectorAsync(document.body, '#login-form_remember');
+    if (!rememberMeInput.checked) rememberMeInput.click();
+    await whenInputCompletedAsync(await whenElementQuerySelectorAsync(document.body, '#login-form_username'));
+    document.querySelector('[data-testid=CredentialsLoginButton]').click();
+  })();
+  // Password
+  (async () => {
+    await whenInputCompletedAsync(await whenElementQuerySelectorAsync(document.body, '#login-form_password'));
+    document.querySelector('[data-testid=CredentialsLoginButton]').click();
+  })();
+}
+if (/^[^/]+\/\/[^/]+\/[^/]+\/mfa\/enter(?:$|\?)/.test(document.URL)) {
+  // Authenticator choices: Text Message, Voice Call, Authenticator. Assuming the last option is always Authenticator which is the
+  // preferred option.
+  (async () => {
+    const options = await whenElementQuerySelectorAsync(document.body, '[data-testid=ResendCodeVerificationOptions]');
+    options.querySelector(':scope > button:last-of-type').click();
+  })();
+}
+if (/^[^/]+\/\/[^/]+\/[^/]+\/mfa\/enter\/app(?:$|\?)/.test(document.URL)) {
+  // Authenticator entry.
+  (async () => {
+    const iTrustThisComputerInput = await whenElementQuerySelectorAsync(document.body, '#mfa-phone-code-form_trustThisDevice');
+    if (!iTrustThisComputerInput.checked) iTrustThisComputerInput.click();
+    await whenInputCompletedAsync(await whenElementQuerySelectorAsync(document.body, '#code'));
+    document.querySelector('[data-testid=SubmitButton]').click();
+  })();
+}
